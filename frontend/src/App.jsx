498b0abf3f51ea4c48fd243a61115332a9cc17b1
suraj <alphaguy433@gmail.com>
2024-09-27 23:33:16 +0530

    Server-Client setup, file structure, packages, done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h3 className='bg-yellow-400 text-red-500'>Chat web app</h3>
+    </>
+  )
+}
+
+export default App
